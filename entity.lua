29d7944ecfe9c82efdb9c4fecf44f8dbf1512f37
gelaw <securegelaw@gmail.com>
2022-04-29 11:17:56 +0200

    factorised player setup, changed player death to spawn a ghost who possess the first dead enemy it touches, changed IA implementation to ease control transfer from IA to player, gameplay and comestic tweaks

--- entity.lua
+++ entity.lua
@@ -4,7 +4,7 @@ function newEntity()
     --display
     shape = "rectangle",
     color = {1, .7, .9},
-    x=10, y=10, width=5, height=5, angle=0,
+    x=10, y=10, z=0, width=5, height=5, angle=0,
     updates = {},
     update = function (self, dt)
       for u, updateFunction in pairs(self.updates) do
@@ -123,7 +123,7 @@ function newAbility()
   }
 end
 
-function ableEntity(entity)
+function ableEntityInit(entity)
   local entity = entity or newEntity()
 
   entity.abilities = {}
@@ -143,11 +143,153 @@ function ableEntity(entity)
         if ability.activeTimer <= 0 then
           ability:deactivate(entity)
         end
-      elseif ability.charges > 0 and ability:bindCheck() and ability:castConditions(entity) ~= nil then
-        ability:activate(entity)
+      elseif ability.charges > 0  and ability:castConditions(entity) ~= nil then
+        if (player == entity and ability:bindCheck()) or (entity.IA and a==entity.IA.task) then
+          ability:activate(entity)
+        end
       end
     end
   end)
+  return entity
+end
+
+function playerInit(entity)
+  local entity = entity or newEntity()
+
+  applyParams(entity, {
+    draw = function (self)
+      love.graphics.translate(self.x, self.y)
+      love.graphics.rotate(self.angle)
+      --shadow display
+      love.graphics.setColor(0, 0, 0, 0.1)
+      love.graphics.rectangle("fill", -self.width/2, -self.height/2, self.width, self.height)
+      --body display (flickering in case of invicibility frames)
+      love.graphics.rotate(-self.angle)
+      love.graphics.translate(0, -1-self.z)
+      love.graphics.rotate(self.angle)
+      love.graphics.scale(1+self.z*0.3)
+      if not self.invicibility or (math.floor(self.invicibility.time*20))%2~=1 then
+          -- jump calculations
+        love.graphics.setColor(self.color)
+        love.graphics.rectangle("fill", -self.width/2, -self.height/2, self.width, self.height)
+        love.graphics.setColor(0, .5, .8, .5)
+        love.graphics.polygon("fill", 2, 0, -3, -2, -3, 2)
+      end
+    end,
+    life = 10,
+    team = 1,
+    onHit = function (self)
+      cameraShake(20, .5)
+      if joystick and joystick:isVibrationSupported() then
+        joystick:setVibration(.05, .05, .2 )
+      end
+    end,
+    IA = nil,
+    onDeath = function (self)
+      self.update = nil
+      self.collide = nil
+      self.color = {.7, .1, .1}
+      table.insert(particuleEffects, {x=self.x, y=self.y, color = {.6, .2, .2}, nudge = 5, size = 3, timeLeft = 1})
+      --ghost out the shell
+      player = {
+        shape = "rectangle",
+        x=self.x, y=self.y,
+        width = 10, height = 10,
+        color = {.3, .3, 1},
+        speed={x=0, y=0},
+        speedDrag= 0.9,
+        maxAcceleration = 3000,
+        update = function (self,  dt)
+          --temporary acceleration variables
+          local ax, ay = 0, 0
+          --if gamepad is connected, use left joystick as input
+          if joystick then
+            a1, a2, a3 = joystick:getAxes()
+            --check if joystick is outside of deadzone (TODO later: parameter 0.3 to be extrated and made modifiable once parameter norm in place)
+            if math.abs(a1)>0.3 or math.abs(a2)>0.3 then
+              ax, ay = self.maxAcceleration*a1*math.abs(a1), self.maxAcceleration*a2*math.abs(a2)
+            end
+          end
+          --keyboard ZQSD binds for player movement
+          if love.keyboard.isDown("z") and not love.keyboard.isDown("s") then
+            ay = -self.maxAcceleration
+          end
+          if love.keyboard.isDown("s") and not love.keyboard.isDown("z") then
+            ay = self.maxAcceleration
+          end
+          if love.keyboard.isDown("q") and not love.keyboard.isDown("d") then
+            ax = -self.maxAcceleration
+          end
+          if love.keyboard.isDown("d") and not love.keyboard.isDown("q") then
+            ax = self.maxAcceleration
+          end
+          --speed, position and orientation calculations
+          self.speed.x = (self.speed.x + ax*dt)*self.speedDrag
+          self.speed.y = (self.speed.y + ay*dt)*self.speedDrag
+          if math.abs(self.speed.x)>0 or math.abs(self.speed.y)>0 then
+            self.angle = -math.atan2(self.speed.x, self.speed.y)+math.rad(90)
+          end
+          local newPosition = {x = self.x + self.speed.x * dt, y= self.y + self.speed.y * dt}
+          self.x = newPosition.x
+          self.y = newPosition.y
+          camera.x = (camera.x + self.x)/2
+          camera.y = (camera.y + self.y)/2
+        end,
+        collide = function (self, collider)
+          if collider.abilities and collider.life and collider.life <= 0 then
+            --kill the ghost
+            player.terminated = true
+            --take control of the enemy
+            player = playerInit(collider)
+            camera.mode = {"follow", player}
+          end
+        end
+      }
+      camera.mode = {"follow", player}
+      table.insert(entities, player)
+    end
+  })
+  table.insert(entity.updates,
+    function (self, dt)
+      --temporary acceleration variables
+      local ax, ay = 0, 0
+      --if gamepad is connected, use left joystick as input
+      if joystick then
+        a1, a2, a3 = joystick:getAxes()
+        --check if joystick is outside of deadzone (TODO later: parameter 0.3 to be extrated and made modifiable once parameter norm in place)
+        if math.abs(a1)>0.3 or math.abs(a2)>0.3 then
+          ax, ay = self.maxAcceleration*a1*math.abs(a1), self.maxAcceleration*a2*math.abs(a2)
+        end
+      end
+      --keyboard ZQSD binds for player movement
+      if love.keyboard.isDown("z") and not love.keyboard.isDown("s") then
+        ay = -self.maxAcceleration
+      end
+      if love.keyboard.isDown("s") and not love.keyboard.isDown("z") then
+        ay = self.maxAcceleration
+      end
+      if love.keyboard.isDown("q") and not love.keyboard.isDown("d") then
+        ax = -self.maxAcceleration
+      end
+      if love.keyboard.isDown("d") and not love.keyboard.isDown("q") then
+        ax = self.maxAcceleration
+      end
+      --speed, position and orientation calculations
+      self.speed.x = (self.speed.x + ax*dt)*self.speedDrag
+      self.speed.y = (self.speed.y + ay*dt)*self.speedDrag
+      if math.abs(self.speed.x)>0 or math.abs(self.speed.y)>0 then
+        self.angle = -math.atan2(self.speed.x, self.speed.y)+math.rad(90)
+      end
+
+      --camera zoom control
+      -- variables set in "cameraSetup()" method
+      if (joystick and joystick:isDown(3)) or love.keyboard.isDown("lctrl") then
+        camera.scale = math.max(camera.scale - camera.scaleChangeRate*dt, camera.minScale)
+      else
+        camera.scale = math.min(camera.scale + camera.scaleChangeRate*dt, camera.maxScale)
+      end
+    end
+  )
   return entity
 end
 
--- main.lua
+++ main.lua
@@ -11,21 +11,25 @@ function test()
     --lifebar display
     love.graphics.translate(player.x, player.y)
     love.graphics.translate(-15, -15)
-    love.graphics.setColor(.1, .1, .2, .3)
-    love.graphics.rectangle("fill", 3, 0, 3*10, 5)
-    love.graphics.setColor(.1, .8, .2, .7)
-    love.graphics.rectangle("fill", 3, 0, 3*player.life, 5)
+    if player.life then
+      love.graphics.setColor(.1, .1, .2, .3)
+      love.graphics.rectangle("fill", 3, 0, 3*10, 5)
+      love.graphics.setColor(.1, .8, .2, .7)
+      love.graphics.rectangle("fill", 3, 0, 3*player.life, 5)
+    end
     --dash charges display
-    for i = 1, player.abilities.dash.maxCharges do
-      if player.abilities.dash.charges < i then
-        love.graphics.setColor(.1, .1, .2, .3)
-      else
-        love.graphics.setColor(0, .3, .9)
-      end
-      love.graphics.circle("fill", 4+10*(i-1), 5, 3)
-      if player.abilities.dash.charges == i - 1 then
-        love.graphics.setColor(0, .3, .9)
-        love.graphics.arc("fill", 4+10*(i-1), 5, 3, -math.pi/2+ 2*math.pi*(1-player.abilities.dash.cooldown/player.abilities.dash.baseCooldown), -math.pi/2)
+    if player.abilities and player.abilities.dash then
+      for i = 1, player.abilities.dash.maxCharges do
+        if player.abilities.dash.charges < i then
+          love.graphics.setColor(.1, .1, .2, .3)
+        else
+          love.graphics.setColor(0, .3, .9)
+        end
+        love.graphics.circle("fill", 4+10*(i-1), 5, 3)
+        if player.abilities.dash.charges == i - 1 then
+          love.graphics.setColor(0, .3, .9)
+          love.graphics.arc("fill", 4+10*(i-1), 5, 3, -math.pi/2+ 2*math.pi*(1-player.abilities.dash.cooldown/player.abilities.dash.baseCooldown), -math.pi/2)
+        end
       end
     end
     --position display
@@ -36,6 +40,7 @@ function test()
   end, 8)
   --abilities hud
   addDrawFunction( function ()
+    if not player.abilities then return end
     love.graphics.origin()
     love.graphics.translate(20, height - 150)
     for a, ability in pairs(player.abilities) do
@@ -147,30 +152,12 @@ function start()
   levelSetup()
   cameraSetup()
   collectibles = 0
-  player = applyParams(ableEntity(livingEntityInit(movingEntityInit())),  {
+  player = applyParams(playerInit(ableEntityInit(livingEntityInit(movingEntityInit()))),  {
     --display
     shape = "rectangle",
     color = {.4, .6, .2},
-    x=10, y=10, z = 0,
-    draw = function (self)
-      love.graphics.translate(self.x, self.y)
-      love.graphics.rotate(self.angle)
-      --shadow display
-      love.graphics.setColor(0, 0, 0, 0.1)
-      love.graphics.rectangle("fill", -self.width/2, -self.height/2, self.width, self.height)
-      --body display (flickering in case of invicibility frames)
-      love.graphics.rotate(-self.angle)
-      love.graphics.translate(0, -1-self.z)
-      love.graphics.rotate(self.angle)
-      love.graphics.scale(1+self.z*0.3)
-      if not self.invicibility or (math.floor(self.invicibility.time*20))%2~=1 then
-          -- jump calculations
-        love.graphics.setColor(self.color)
-        love.graphics.rectangle("fill", -self.width/2, -self.height/2, self.width, self.height)
-        love.graphics.setColor(0, .5, .8, .5)
-        love.graphics.polygon("fill", 2, 0, -3, -2, -3, 2)
-      end
-    end,
+    x=10, y=10,
+
     -- actions
     abilities = {
       dash = applyParams(newAbility(), {
@@ -346,60 +333,9 @@ function start()
       })
     },
     --gameplay(?)
-    team = 1,
-    onHit = function (self)
-      cameraShake(20, .5)
-      if joystick and joystick:isVibrationSupported() then
-        joystick:setVibration(.05, .05, .2 )
-      end
-    end,
-    onDeath = function (self)
-      self.update = nil
-      self.collide = nil
-      self.color = {.7, .1, .1}
-      table.insert(particuleEffects, {x=self.x, y=self.y, color = {.6, .2, .2}, nudge = 5, size = 3, timeLeft = 1})
-    end
-  })
-  table.insert(player.updates,
-    function (self, dt)
-      --temporary acceleration variables
-      local ax, ay = 0, 0
-      --if gamepad is connected, use left joystick as input
-      if joystick then
-        a1, a2, a3 = joystick:getAxes()
-        --check if joystick is outside of deadzone (TODO later: parameter 0.3 to be extrated and made modifiable once parameter norm in place)
-        if math.abs(a1)>0.3 or math.abs(a2)>0.3 then
-          ax, ay = self.maxAcceleration*a1*math.abs(a1), self.maxAcceleration*a2*math.abs(a2)
-        end
-      end
-      --keyboard ZQSD binds for player movement
-      if love.keyboard.isDown("z") and not love.keyboard.isDown("s") then
-        ay = -self.maxAcceleration
-      end
-      if love.keyboard.isDown("s") and not love.keyboard.isDown("z") then
-        ay = self.maxAcceleration
-      end
-      if love.keyboard.isDown("q") and not love.keyboard.isDown("d") then
-        ax = -self.maxAcceleration
-      end
-      if love.keyboard.isDown("d") and not love.keyboard.isDown("q") then
-        ax = self.maxAcceleration
-      end
-      --speed, position and orientation calculations
-      self.speed.x = (self.speed.x + ax*dt)*self.speedDrag
-      self.speed.y = (self.speed.y + ay*dt)*self.speedDrag
-      if math.abs(self.speed.x)>0 or math.abs(self.speed.y)>0 then
-        self.angle = -math.atan2(self.speed.x, self.speed.y)+math.rad(90)
-      end
 
-      --camera zoom control
-      -- variables set in "cameraSetup()" method
-      if (joystick and joystick:isDown(3)) or love.keyboard.isDown("lctrl") then
-        camera.scale = math.max(camera.scale - camera.scaleChangeRate*dt, camera.minScale)
-      else
-        camera.scale = math.min(camera.scale + camera.scaleChangeRate*dt, camera.maxScale)
-      end
-    end)
+  })
+
   table.insert(entities, player)
 
   --use in base camera.update()
@@ -410,7 +346,7 @@ function start()
   --ennemy spawn
   for i = 1, 10 do
     local type = math.random(2)
-    local ennemy = applyParams(livingEntityInit(movingEntityInit()),
+    local ennemy = applyParams(ableEntityInit(livingEntityInit(movingEntityInit())),
       {
         color = (type == 1 and  {.1, .2, .9} or {.9, .3, .1}),
         ignoreWalls = type == 1,
@@ -425,8 +361,50 @@ function start()
           love.graphics.scale(2/camera.scale)
           love.graphics.print(self.IA.task)
         end,
-        speedDrag= math.random(0, 8)/10, maxAcceleration = math.random(45, 60)*type,
+         maxAcceleration = math.random(1200, 1500)*type,
         maxSpeed = 100,
+        abilities = {
+          shoot = applyParams(newAbility(), {
+            baseCooldown = math.random(1, 9),
+            displayOnUI = function (self)
+              love.graphics.setColor(.2, .2, .2)
+              love.graphics.rectangle("fill", 0, 0, 130, 130)
+              love.graphics.setColor(1, 1, 1, .1)
+              love.graphics.rectangle("fill", 0, 0, 130, 130 * (1-self.cooldown/self.baseCooldown))
+            end,
+            activate = function (self, caster)
+
+              --spawn projectile entity
+              projectile = {
+                  shape = "rectangle",
+                  color = {0, 0, 0},
+                  x=caster.x + math.cos(caster.angle)*5,
+                  y=caster.y + math.sin(caster.angle)*5,
+                  width=5, height=1, angle=caster.angle, speed = 300,
+                  timer = 0,
+                  update = function (self, dt)
+                    self.timer = self.timer + dt
+                    if self.timer > 4 then self.terminated = true return end
+                    self.x = self.x + math.cos(self.angle)*dt*self.speed
+                    self.y = self.y + math.sin(self.angle)*dt*self.speed
+                  end,
+                  contactDamage = 2,
+                  team = caster.team,
+                  collide = function (self, collider)
+                    if self.team and collider.team and collider.team ~= self.team then
+                      self.terminated = true
+                    end
+                  end
+              }
+
+              table.insert(entities, projectile)
+              self.active = true
+            end,
+            bindCheck = function ()
+              return (joystick and joystick:isDown(4)) or love.keyboard.isDown("e")
+            end
+          })
+        },
         --behavior
         IA = {
           --default task
@@ -437,8 +415,8 @@ function start()
             --turn toward player
             entity.angle = -math.atan2(entity.x - player.x, entity.y - player.y)-math.rad(90)
             --math stuff for movement
-            entity.speed.x = entity.speed.x + entity.maxAcceleration*math.cos(entity.angle)*dt
-            entity.speed.y = entity.speed.y + entity.maxAcceleration*math.sin(entity.angle)*dt
+            entity.speed.x = (entity.speed.x + entity.maxAcceleration*math.cos(entity.angle)*dt)*entity.speedDrag
+            entity.speed.y = (entity.speed.y + entity.maxAcceleration*math.sin(entity.angle)*dt)*entity.speedDrag
 
             --check if player is close
             if (math.abs(entity.x - player.x) < self.shootDistance and math.abs(entity.y -  player.y) < self.shootDistance) then
@@ -450,7 +428,6 @@ function start()
             --math stuff for movement
             entity.speed.x = entity.speed.x * entity.speedDrag
             entity.speed.y = entity.speed.y * entity.speedDrag
-            local newPosition = {x = entity.x + entity.speed.x * dt, y= entity.y + entity.speed.y * dt}
             --check if speed is close to 0
             if math.abs(entity.speed.x) < 0.1 and math.abs(entity.speed.y) < 0.1 then
               -- switch to "shoot" task
@@ -460,43 +437,17 @@ function start()
           shoot = function (self, entity, dt)
             --turn toward player
             entity.angle = -math.atan2(entity.x - player.x, entity.y - player.y)-math.rad(90)
-            --spawn projectile entity
-            local projectile = {
-                shape = "rectangle",
-                color = {0, 0, 0},
-                x=entity.x + math.cos(entity.angle)*5,
-                y=entity.y + math.sin(entity.angle)*5,
-                width=5, height=1, angle=entity.angle, speed = 300,
-                timer = 0,
-                update = function (self, dt)
-                  self.timer = self.timer + dt
-                  if self.timer > 4 then self.terminated = true return end
-                  self.x = self.x + math.cos(self.angle)*dt*self.speed
-                  self.y = self.y + math.sin(self.angle)*dt*self.speed
-                end,
-                contactDamage = 2,
-                team = 2,
-                collide = function (self, collider)
-                  if collider.team and collider.team < 2 then
-                    self.terminated = true
-                  end
-                end
-            }
-            table.insert(entities, projectile)
             --init a cooldown timer and switch to reload task
-            self.shootCooldownTimer = self.shootCooldown
-            self.task = "reload"
+            if entity.abilities.shoot.cooldown > 0 then
+              self.task = "reload"
+            end
           end,
           reload = function (self, entity, dt)
-            --cooldown countdown
-            self.shootCooldownTimer = self.shootCooldownTimer - dt
-            if self.shootCooldownTimer <= 0 then
-              --switch to "follow" task if player is far or "shoot" task otherwise
-              if (math.abs(entity.x - player.x) > self.followDistance or math.abs(entity.y -  player.y) > self.followDistance) then
-                self.task = "follow"
-              else
-                self.task = "shoot"
-              end
+            if entity.abilities.shoot.cooldown > 0 then return end
+            if (math.abs(entity.x - player.x) > self.followDistance or math.abs(entity.y -  player.y) > self.followDistance) then
+              self.task = "follow"
+            else
+              self.task = "shoot"
             end
           end
         },
@@ -504,9 +455,9 @@ function start()
         team = 2,
         life = 1,
         onDeath = function (self)
-          self.update = nil
           self.IA.task = "dead"
-          self.collide = nil
+          self.contactDamage = nil
+          self.speed = {x=0,y=0}
           self.color = {.3, .3, .3}
           table.insert(particuleEffects, {x=self.x, y=self.y, color = {.6, .2, .2}, nudge = 5, size = 3, timeLeft = 1})
         end
@@ -514,7 +465,9 @@ function start()
     )
     table.insert(ennemy.updates,
       function (self, dt)
-        self.IA[self.IA.task](self.IA, self, dt)
+        if self.IA and self.IA[self.IA.task] then
+          self.IA[self.IA.task](self.IA, self, dt)
+        end
       end)
     table.insert(entities, ennemy)
   end
@@ -559,11 +512,6 @@ function wallCollision(start, destination)
 end
 
 function love.joystickpressed(joystick, button)
-  if player.life <= 0 then
-    entities = {}
-    table.insert(entities, player)
-    start()
-  end
 end
 
 function love.keypressed(key, scancode, isrepeat)
@@ -576,10 +524,5 @@ function love.keypressed(key, scancode, isrepeat)
   end
   if key == "h" then
     showHitboxes = not showHitboxes
-  end
-  if player.life <= 0 then
-    entities = {}
-    table.insert(entities, player)
-    start()
   end
 end
